f6a25d957dd46860975f6da7cb2c33733607d969
arun1111j <justarun0210@gmail.com>
2025-07-16 16:22:28 +0530

    success

--- 212222040015/auth.js
+++ 212222040015/auth.js
@@ -1,16 +1,15 @@
 // register.js
 import fetch from 'node-fetch';
-const authURL = 'http://20.244.56.144/evaluation-service/register';
+const authURL = 'http://20.244.56.144/evaluation-service/auth';
 
 const authenticationData = {
     email: "justarun0210@gmail.com",
     name: "arun j",
-    mobileNo: "9384149692",
-    githubUsername: "arun1111j",
     rollNo: "212222040015",
     accessCode: 'qguCff',
+    clientID: "26595c82-056b-40a9-b9a1-9de6d267186e",
+    clientSecret: "fMhnGZsHCtbkKVSc"
 };
-
 async function authLogger() {
     try {
         const response = await fetch(authURL, {
@@ -22,12 +21,13 @@ async function authLogger() {
         });
 
         if (!response.ok) {
+
             throw new Error(`Authentication failed with status ${response.status}`);
         }
 
         const data = await response.json();
-        console.log(`ClientID: ${data.clientID}`);
-        console.log(`ClientSecret: ${data.clientSecret}`);
+        console.log(`token_type: ${data.token_type}`);
+        console.log(`access_token: ${data.access_token}`);
         console.log(`Expires_in: ${data.expires_in}`);
     } catch (error) {
         console.error('❌ Error registering:', error.message);
